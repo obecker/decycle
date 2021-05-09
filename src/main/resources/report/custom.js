9a37bbe021a76b23694a923abd7519b496da1bc8
Oliver Becker <ob@obqo.de>
2021-05-09 19:24:27 +0200

    Change highlighting of same classes in HTML report

--- src/main/resources/report/custom.js
+++ src/main/resources/report/custom.js
@@ -3,13 +3,13 @@ $(function() {
     $(this).parent().find('ul.class-references').slideToggle();
   });
 
-  let h = function(span, color, weight) {
-    $('span.class-node[data-name="' + $(span).data('name') + '"]').css('color', color).css('font-weight', weight);
+  let h = function(span) {
+    return $('span.class-node[data-name="' + $(span).data('name') + '"]');
   };
 
   $('span.class-node').mouseover(function() {
-    h(this, 'black', 'bold');
+    h(this).addClass('highlight');
   }).mouseout(function() {
-    h(this, 'dimgray', 'normal')
-  })
+    h(this).removeClass('highlight');
+  });
 });
