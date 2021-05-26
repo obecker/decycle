d3a789dfbb53a9b2e151f335bd4885e64829903f
Oliver Becker <ob@obqo.de>
2021-05-26 20:00:38 +0200

    :arrow_up: Upgrade to Gradle 6.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ group = "de.obqo.decycle"
 version = "0.2.0-SNAPSHOT"
 
 tasks.wrapper {
-    gradleVersion = "6.8.3"
+    gradleVersion = "6.9"
     distributionType = Wrapper.DistributionType.ALL
 }
 
