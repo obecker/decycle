56491ee044c8af477529b6427179abb4c5304595
Oliver Becker <ob@obqo.de>
2022-12-07 20:25:00 +0100

    :sparkles: Better Kotlin DSL support
    - add Kotlin DSL specific configuration for "ignoring" and "sourceSets"
    - extend README

---
+++ plugin-gradle/demo/test-sources-kt.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("de.obqo.decycle")
+}
+
+decycle {
+    sourceSets(sourceSets.test)
+}
--- plugin-gradle/src/main/java/de/obqo/decycle/gradle/DecycleExtension.java
+++ plugin-gradle/src/main/java/de/obqo/decycle/gradle/DecycleExtension.java
@@ -7,8 +7,11 @@ import java.util.stream.Collectors;
 import org.gradle.api.Action;
 import org.gradle.api.GradleException;
 import org.gradle.api.NamedDomainObjectContainer;
+import org.gradle.api.NamedDomainObjectProvider;
 import org.gradle.api.Project;
 import org.gradle.api.tasks.SourceSet;
+
+import kotlin.Pair;
 
 /**
  * Extension class for the configuration of decycle
@@ -27,9 +30,32 @@ public class DecycleExtension {
         this.configuration = configuration;
     }
 
+    /**
+     * Declare the source sets to checked by decycle. Will be used by the Groovy Gradle DSL.
+     *
+     * @param sourceSets the source sets to be checked
+     */
     public void sourceSets(final SourceSet... sourceSets) {
         for (final SourceSet sourceSet : sourceSets) {
             this.configuration.addSourceSet(sourceSet);
+        }
+    }
+
+    /**
+     * Declare the source sets to checked by decycle. Will be used by the Kotlin Gradle DSL.
+     *
+     * @param sourceSets the providers for the source sets to be checked
+     */
+    public void sourceSets(final NamedDomainObjectProvider<?>... sourceSets) {
+        // don't use NamedDomainObjectProvider<SourceSet> as the compiler can't check the generic type anyway
+        for (final NamedDomainObjectProvider<?> sourceSet : sourceSets) {
+            final Object value = sourceSet.get();
+            if (!(value instanceof SourceSet)) {
+                throw new GradleException(String.format(
+                        "decycle: passed value to sourceSets is not a SourceSet, encountered %s",
+                        value));
+            }
+            this.configuration.addSourceSet((SourceSet) value);
         }
     }
 
@@ -45,12 +71,22 @@ public class DecycleExtension {
         }
     }
 
+    @Deprecated
     public void ignoring(final String... ignoreSpec) {
         throw new GradleException(String.format(
                 "decycle: ignoring must be used with from: and to: values, found %s",
                 String.join(", ", ignoreSpec)));
     }
 
+    /**
+     * Add a dependency that should be ignored. Preferably to be used by the Groovy Gradle DSL:
+     * <pre>
+     *     ignoring from: '...', to: '...'
+     * </pre>
+     *
+     * @param ignoreSpec map containing only "from" and "to" keys for the dependency
+     * @see de.obqo.decycle.slicer.IgnoredDependency#create(String, String)
+     */
     public void ignoring(final Map<String, String> ignoreSpec) {
         final Set<String> ignoreKeys = Set.of("from", "to");
         if (!ignoreKeys.containsAll(ignoreSpec.keySet())) {
@@ -62,6 +98,19 @@ public class DecycleExtension {
                             .collect(Collectors.joining(", "))));
         }
         this.configuration.addIgnoredDep(new IgnoreConfig(ignoreSpec.get("from"), ignoreSpec.get("to")));
+    }
+
+    /**
+     * Add a dependency that should be ignored. Preferably to be used by the Kotlin Gradle DSL:
+     * <pre>
+     *     ignoring("..." to "...")
+     * </pre>
+     *
+     * @param ignoreSpec a pair representing the dependency
+     * @see de.obqo.decycle.slicer.IgnoredDependency#create(String, String)
+     */
+    public void ignoring(final Pair<String, String> ignoreSpec) {
+        this.configuration.addIgnoredDep(new IgnoreConfig(ignoreSpec.getFirst(), ignoreSpec.getSecond()));
     }
 
     public void slicings(final Action<NamedDomainObjectContainer<SlicingExtension>> action) {
