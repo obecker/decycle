b4fb35c97d5ed4bc3992f692120eed416e0df040
Oliver Becker <ob@obqo.de>
2024-01-21 17:42:12 +0100

    :hammer: Add Gradle wrapper validation, Use slf4j-bom

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -16,7 +16,8 @@ dependencies {
     implementation(libs.guava)
     implementation(libs.j2html)
     implementation(libs.maven.artifact)
-    implementation(libs.slf4j.api)
+    implementation(platform(libs.slf4j.bom))
+    implementation("org.slf4j:slf4j-api")
     implementation(libs.webjars.locator) {
         exclude(group = "com.fasterxml.jackson.core") // not used
     }
@@ -27,7 +28,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testRuntimeOnly(libs.slf4j.jdk14)
+    testRuntimeOnly("org.slf4j:slf4j-jdk14")
 }
 
 tasks.compileJava {
