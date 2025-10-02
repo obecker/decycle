a3be6454d4ea0653f7cc8a27e335e8e8bba24187
Oliver Becker <ob@obqo.de>
2025-10-02 09:27:25 +0200

    :bricks: Adjust maven publishing to OSSRH

--- gradle/publishing.gradle.kts
+++ gradle/publishing.gradle.kts
@@ -7,10 +7,11 @@ configure<PublishingExtension> {
             url = file("${rootProject.buildDir}/repository").toURI()
         }
         maven {
+            // see https://central.sonatype.org/publish/generate-portal-token/
             val ossrhUsername: String? by project
             val ossrhPassword: String? by project
             name = "sonatype"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
             credentials {
                 username = ossrhUsername
                 password = ossrhPassword
@@ -65,4 +66,29 @@ tasks.withType<Javadoc>() {
         file("${destinationDir}/search.js").appendText("getURLPrefix = function(ui) { return ''; };\n")
     }
     (options as StandardJavadocDocletOptions).links("https://docs.oracle.com/en/java/javase/11/docs/api/")
+}
+
+val postToSonatype by tasks.registering {
+    group = "publishing"
+    description = "Finalizes the publication by sending a POST request to OSSRH"
+    // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#1-modify-your-ci-script
+    doLast {
+        val ossrhUsername: String? by project
+        val ossrhPassword: String? by project
+        val ossrhNamespace = "de.obqo"
+        val url = "https://ossrh-staging-api.central.sonatype.com/manual/upload/defaultRepository/$ossrhNamespace"
+        val authToken = java.util.Base64.getEncoder().encodeToString("$ossrhUsername:$ossrhPassword".toByteArray())
+        val authHeader = "Authorization: Bearer $authToken"
+        val process = ProcessBuilder("curl", "-X", "POST", "-H", authHeader, url)
+            .inheritIO()
+            .start()
+        val exitCode = process.waitFor()
+        if (exitCode != 0) {
+            throw GradleException("POST request to OSSRH failed (exit code: $exitCode)")
+        }
+    }
+}
+
+tasks.matching { it.name == "publishToSonatype" }.configureEach {
+    finalizedBy(postToSonatype)
 }
